499e5ed1853e564cc5a7f1ac9ce452dedb31053d
joyzoso <joyrockon@gmail.com>
2016-10-29 15:58:07 -0700

    added space

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,9 +18,9 @@ var Nav = () => {
   return (
     <div>
     <h2>Nav Component</h2>
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Check Yo Weather</IndexLink>
-    <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>What's this about?</Link>
-    <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Check it out!</Link>
+    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Check Yo Weather </IndexLink>
+    <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>What's this about? </Link>
+    <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Check it out! </Link>
     </div>
   );
 };
