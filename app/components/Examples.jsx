ae649bf7e5042ef00a392b9b105e696129717d02
joyzoso <joyrockon@gmail.com>
2016-10-29 16:14:14 -0700

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
+    <div>
     <h3>Examples Component!</h3>
+    <p>Zeppelin Rules!</p>
+    </div>
   )
 };
 module.exports = Examples;
